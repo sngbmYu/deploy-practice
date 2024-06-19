20191eeef414e4b057ccbf06cc17d4fda17f1366
yu seungbeom <dldkdlelqnzo@gmail.com>
2024-06-19 16:20:02 +0900

    init: 프로젝트 생성 완료

---
+++ src/main/java/com/ktb/deploypractice/DeployPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.deploypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployPracticeApplication.class, args);
+    }
+
+}
